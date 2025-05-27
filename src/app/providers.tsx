8085c42e61cae97ea4784d65fb51df3f8d16e0a8
JayWebtech <jethroadamzy@gmail.com>
2025-05-27 19:28:05 +0100

    fixes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -28,7 +28,8 @@ export function Providers({ children }: ProvidersProps) {
 
   const rpc = useCallback((chain: Chain) => {
     return {
-      nodeUrl: `https://starknet-sepolia.drpc.org`,
+      //nodeUrl: `https://starknet-sepolia.drpc.org`,
+      nodeUrl: `https://starknet.api.onfinality.io/public`
     };
   }, []);
 
